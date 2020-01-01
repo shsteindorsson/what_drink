20925d0237474a0efb567c7e5ee129995e8025f9
Sverrir Steindorsson <sverrir@mykolab.ch>
2020-01-01 22:00:48 +0100

    Remove cucumber file & update project names

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fridge_production"
+  # config.active_job.queue_name_prefix = "what_drink_production"
 
   config.action_mailer.perform_caching = false
 
